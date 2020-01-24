2985d6772b5635d0e3c7438bc9f9ec3dc9eadd89
Phong Yang <phongyang88@hotmail.com>
2020-01-24 11:23:02 -0600

    newly added smurfs render to page

--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
 import Axios from "axios";
+
+import SmurfsContext from "../contexts/SmurfsContext";
 
 const AddSmurf = () => {
     const [name, setName] = useState("");
     const [age, setAge] = useState("");
     const [height, setHeight] = useState("");
+
+    const {smurfsList, setSmurfsList} = useContext(SmurfsContext);
 
     const handleNameChanges = e => {
         setName([e.target.value]);
@@ -21,14 +25,17 @@ const AddSmurf = () => {
     const handleAddSmurf = (name, age, height) => {
         const newSmurf = {
             name: name,
-            age: parseInt(age),
+            age: Math.round(age),
             height: height,
             id: Date.now()
         }
 
         Axios.post(`http://localhost:3333/smurfs`, newSmurf)
         .then(res => {
-            console.log(res)
+            setSmurfsList(res.data)
+            setName("")
+            setAge("")
+            setHeight("")
         })
         .catch(err => {
             console.log(err)
@@ -38,7 +45,7 @@ const AddSmurf = () => {
     return (
         <div>
             <input type="text" value={name} onChange={handleNameChanges} placeholder="Enter Name"></input>
-            <input type="text" value={age} onChange={handleAgeChanges} placeholder="Enter Age"></input>
+            <input type="number" value={age} onChange={handleAgeChanges} placeholder="Enter Age"></input>
             <input type="text" value={height} onChange={handleHeightChanges} placeholder="Enter Height"></input>
             <button onClick={()=> handleAddSmurf(name,age,height)}>Add Smurf</button>
         </div>
